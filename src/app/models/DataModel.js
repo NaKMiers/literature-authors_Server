c39599fa29d1f39bfc44b56b4ebe0aa3915e1368
AnK <56575187+NaKMiers@users.noreply.github.com>
2022-05-07 21:25:42 +0700

    change construc of model

--- src/app/models/DataModel.js
+++ src/app/models/DataModel.js
@@ -1,18 +1,3 @@
-// const mongoose = require('mongoose')
-// const Schema = mongoose.Schema
-
-// const BlogModel = new Schema(
-//    {
-//       userId: String,
-//       statusText: String,
-//       hearts: [String],
-//       type: { type: String, default: 'blog' },
-//    },
-//    { timestamps: true }
-// )
-
-// module.exports = mongoose.model('Blogs', BlogModel)
-
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
@@ -21,13 +6,7 @@ const DataModel = new Schema(
       lesson: String,
       author: String,
       content: String,
-      bg1: String,
-      bg2: String,
-      bg3: String,
-      color1: String,
-      color2: String,
-      color3: String,
-      width: Number,
+      image: String,
    },
    {
       timestamps: true,
